3762b10bf1fce205110ec69cf046c9918624ada9
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-05 01:21:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
     sql = <<-SQL
-      SELECT * FROM students 
+      SELECT * FROM students
       WHERE name = ?
       SQL
 
