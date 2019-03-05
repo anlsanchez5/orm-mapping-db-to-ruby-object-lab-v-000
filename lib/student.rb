0dfce8b80f327e2323723f2aeb80495f6a17fd12
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-05 01:50:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,15 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.all_students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade < '12'
+      SQL
+
+    DB[:conn].execute(sql)
+  end
+
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
